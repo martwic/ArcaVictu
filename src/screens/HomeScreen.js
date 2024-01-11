893c322d664028dfe0ce05dd318e08bbe08f29b5
martwic <72266358+martwic@users.noreply.github.com>
2024-01-11 23:56:35 +0100

    shopping list

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import { View, Text, ScrollView, BackHandler } from 'react-native';
+import React, {useContext, useEffect, useState} from 'react';
+import { View, Text, ScrollView, BackHandler, FlatList } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import {CalendarDaysIcon} from 'react-native-heroicons/outline';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
-
+import { PageContext } from '../constants/pageContext';
+import { supabase } from '../constants';
+import { useNavigation } from '@react-navigation/native';
 
 export default function HomeScreen(){
+    const navigation = useNavigation();
+    const[dishesList, setDishes]=useState('');
+    const[mealsList, setMeals]=useState('');
+    const [userId] = useContext(PageContext);
+    useEffect(() => {
+        const unsubscribe = navigation.addListener('focus', () => {
+          getMeals()
+          getDishes()
+        });
+        return unsubscribe;
+      }, [navigation]);
     BackHandler.addEventListener("hardwareBackPress", ()=>true)
     var date = new Date;
     var days = ['Niedziela','Poniedziałek','Wtorek','Środa','Czwartek','Piątek','Sobota'];
@@ -17,6 +29,52 @@ export default function HomeScreen(){
         var month = new Date().getMonth() + 1;
         return(month>9 ? date + '.' + month : date + '.0' + month );
   }
+  
+  const getCurrentDateDatabase=()=>{
+ 
+    var day = new Date().getDate();
+    var month = new Date().getMonth() + 1;
+    var year = new Date().getFullYear();
+    month = month>9 ? month : '0' + month;
+    day = day>9 ? day : '0' + day;
+    return( year+'-'+month+'-'+day);
+}
+    const getMeals = async ()=>{
+        try {
+            const { data, error, status } = await supabase.from('meals')
+            .select(`id, dishes(account_id, recipe_id, recipes(name)), consumption_date, servings`)
+            .eq('consumption_date',getCurrentDateDatabase())
+            .eq('dishes.account_id',userId)
+            if (error && status !== 406) {
+              throw error
+            }
+            if (data) {
+              setMeals(data)
+            }
+          } catch (error) {
+            if (error instanceof Error) {
+              Alert.alert(error.message)
+            }
+          } 
+    }
+    const getDishes = async ()=>{
+        try {
+            const { data, error, status } = await supabase.from('dishesview')
+            .select('name, preparation_date, sum, account_id')
+            .eq('preparation_date',getCurrentDateDatabase())
+            .eq('account_id',userId)
+            if (error && status !== 406) {
+              throw error
+            }
+            if (data) {
+              setDishes(data)
+            }
+          } catch (error) {
+            if (error instanceof Error) {
+              Alert.alert(error.message)
+            }
+          } 
+    }
     return (
         <SafeAreaView className="flex-1 justify-center items-center bg-[#FFF6DC]">
                 <View className="bg-[#FFC6AC] w-full p-2 items-center">
@@ -25,13 +83,41 @@ export default function HomeScreen(){
                 <View className="flex-1">
                     <Text className="font-['Gothic'] font-bold" style={{fontSize:hp(3)}}>Gotowanie</Text>
                     <View>
-
+                    <FlatList
+                        ListEmptyComponent={null}
+                        scrollEnabled
+                        data={dishesList}
+                        keyExtractor={item => item.id} 
+                        renderItem={({item}) => 
+                        <>
+                            <Text style={{fontSize:hp(2.3),padding:hp(0.5), textAlign:'center'}} onPress={()=> navigation.navigate('RecipeDetail', {...item})}>
+                            {item.name}
+                            </Text>
+                            <Text>{item.sum}</Text>
+                        </>//id, dishes(recipes(id, name), account_id), consumption_date, servings
+                        }
+                    />
                     </View>
                 </View>
                 <View className="border-b-2 border-[#FFC6AC] w-4/5"/>
                 <View className="flex-1 p-2">
                     <Text className="font-['Gothic'] font-bold" style={{fontSize:hp(3)}}>Jadłospis</Text>
-                    <View></View>
+                    <View>
+                    <FlatList
+                        ListEmptyComponent={null}
+                        scrollEnabled
+                        data={mealsList}
+                        keyExtractor={item => item.id} 
+                        renderItem={({item}) => 
+                        <>
+                            <Text style={{fontSize:hp(2.3),padding:hp(0.5), textAlign:'center'}} onPress={()=> navigation.navigate('RecipeDetail', {...item})}>
+                            {item.dishes.recipes.name}
+                            </Text>
+                            <Text>{item.servings}</Text>
+                        </>//id, dishes(recipes(id, name), account_id), consumption_date, servings
+                        }
+                    />
+                    </View>
                 </View>
             </SafeAreaView>
     )
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,23 +1,127 @@
-import React from 'react';
-import { View, Text, ScrollView, BackHandler } from 'react-native';
+import React, {useState, useContext, useEffect} from 'react';
+import { View, Text, ScrollView, Modal, TouchableOpacity, TextInput, FlatList } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
-import { DatePickerInput } from 'react-native-paper-dates';
-
+import { Button, Input} from 'react-native-elements'
+import { Calendar, LocaleConfig } from 'react-native-calendars';
+import { PageContext } from '../constants/pageContext';
+import { supabase } from '../constants';
 
 export default function RecipesScreen(){
-    const [inputDate, setInputDate] = React.useState(undefined)
+    const getCurrentDate=()=>{
+ 
+        var day = new Date().getDate();
+        var month = new Date().getMonth() + 1;
+        var year = new Date().getFullYear();
+        month = month>9 ? month : '0' + month;
+        day = day>9 ? day : '0' + day;
+        return( year+'-'+month+'-'+day);
+    }
+    const [userId] = useContext(PageContext);
+    const [dateFrom, setDateFrom] = useState(getCurrentDate())
+    const [openFrom, setOpenFrom] = useState(false)
+    const [openTo, setOpenTo] = useState(false)
+    const [dateTo, setDateTo] = useState(getCurrentDate())
+    const [minDate, setMinDate] = useState(getCurrentDate())
+    const [maxDate, setMaxDate] = useState()
+    const [list, setList] = useState()
+    useEffect(() => {
+          getList()
+      }, []);
+    
+    LocaleConfig.locales.en = LocaleConfig.locales[''];
+        LocaleConfig.locales.pl = {
+          monthNames: [
+            'Styczeń','Luty','Marzec','Kwiecień','Maj','Czerwiec','Lipiec','Sierpień','Wrzesień','Październik','Listopad','Grudzień',
+          ],
+          dayNames: [
+            'Poniedziałek','Wtorek','Środa','Czwartek','Piątek','Sobota','Niedziela',
+          ],
+          dayNamesShort: ['Pn', 'Wt', 'Śr', 'Cz', 'Pt', 'So', 'Nd'],
+        };
+        LocaleConfig.defaultLocale = 'pl';
+
+        const getList = async ()=>{
+            try {
+                const { data, error, status } = await supabase.from('list')
+                .select(`id, name, preparation_date, weight, account_id`)
+                .eq('account_id',userId)
+                .lte('preparation_date', dateTo)
+                .gte('preparation_date', dateFrom)
+                if (error && status !== 406) {
+                  throw error
+                }
+                if (data) {
+                  setList(data)
+                }
+              } catch (error) {
+                if (error instanceof Error) {
+                  Alert.alert(error.message)
+                }
+              } 
+        }
     return (
         <SafeAreaView  className="flex-1 justify-center items-center bg-[#FFF6DC]">
         <View className="bg-[#FFC6AC] w-full p-2 items-center">
             <Text className="font-['Gothic']" style={{fontSize:hp(5)}}>Lista zakupów</Text>
         </View>
         <View className="flex-1 items-center" style={{padding:wp(5)}}>
-            <View className="flex-row">
-                <DatePickerInput locale="pl" label={''} value={inputDate} inputMode='start' onChange={(d) => setInputDate(d)} mode='outlined'/>
-                <Text>  </Text>
-                <DatePickerInput locale="pl" label={''} value={inputDate} inputMode='start' onChange={(d) => setInputDate(d)} mode='outlined'/>
+            <View className="items-center justify-center flex-row">
+            <View className="p-3">
+            <Button  buttonStyle={{backgroundColor:'#b1ae95', width:wp(40)}}title={dateFrom} onPress={() => setOpenFrom(!openFrom)} />
             </View>
+            <View className="p-3">
+            <Button buttonStyle={{backgroundColor:'#b1ae95', width:wp(40)}} title={dateTo} onPress={() => setOpenTo(!openTo)} />
+            </View>
+            </View>
+            <Modal 
+                visible={openFrom}
+                >
+                    <View className="flex-1 bg-[#FFF6DC]">
+                    <Calendar
+                    markedDates={{[dateFrom]: {selected:true, selectedColor:'#b1ae95'}}}
+                    minDate={getCurrentDate()}
+                    maxDate={maxDate}
+                    theme={{calendarBackground: 'transparent', todayTextColor:'#b1ae95', arrowColor:'#b1ae95'}}
+                    onDayPress={day => {
+                        setDateFrom(day.dateString);
+                        setMinDate(day.dateString);
+                        setOpenFrom(!openFrom);
+                        getList()
+                    }}
+                    /></View>
+                 </Modal>
+                 <Modal 
+                visible={openTo}
+                >
+                    <View className="flex-1 bg-[#FFF6DC]">
+                    <Calendar
+                    markedDates={{[dateTo]: {selected:true, selectedColor:'#b1ae95'}}}
+                    minDate={minDate}
+                    theme={{calendarBackground: 'transparent', todayTextColor:'#b1ae95', arrowColor:'#b1ae95'}}
+                    onDayPress={day => {
+                        setDateTo(day.dateString);
+                        setMaxDate(day.dateString);
+                        setOpenTo(!openTo);
+                        getList()
+                    }}
+                    /></View>
+                 </Modal>
+
+                 <FlatList className=""
+                    ListEmptyComponent={null}
+                    data={list}
+                    keyExtractor={(item) => {
+                    return item.id;
+                    }}
+                    renderItem={({item}) => 
+                        <View className="p-3">
+                            <Text 
+                            style={{fontSize:hp(2.5)}}>{item.name} - {item.weight} g</Text>
+                        </View>
+                    }
+                    />
+
         </View>
     </SafeAreaView>
     )
